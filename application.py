4c8710d8fd992c206495bb20f0e9bba8c5abbf9d
Steven Natz <stevennatz@yahoo.com>
2021-04-10 04:38:25 -0700

    First Menu Code Commit

--- application.py
+++ application.py
@@ -1,12 +1,129 @@
 import db_conn, os, hashlib
 
+#EMPLOYEE MENU CODE
 def employeeMenu():
-    #EMPLOYEE MENU CODE HERE
-    print("You are an employee")
+    print("--- Employee Menu ---\n")
+    print("What would you like to do?")
+    print("1. Check-out Equipment")
+    print("2. Return Equipment")
+    print("3. View Your Equipment Records")
+    print("4. Track New Equipment Orders")
+    print("5. Log Out\n")
+    while True:
+        userChoice = input("Enter a menu option: ")
+        if userChoice == '1':
+            equipID = input("Enter Equipment ID Number to check-out: ")
+            checkout(userID, equipID)
+            print("\nThis item has now been checked-out.\n\n")
+            continue
+        elif userChoice == '2':
+            equipID = input("Enter Equipment ID Number to check-in: ")
+            returns(userID, equipID)
+            print("\nThis item has now been returned.\n\n")
+            continue
+        elif userChoice == '3':
+            print("Your Equipment Records:")
+            getUser(userID)
+            print()
+            continue
+        elif userChoice == '4':
+            print("All New Equipment Orders:")
+            generateReport(reportType, userID = None)
+            print()
+            continue
+        elif userChoice == '5':
+            cont = input("\nLog Out? (y/n): ").lower()
+            while True:
+                if cont == "y":
+                    print("\nSuccessfully logged out.\n\n")
+                    main()
+                elif cont == "n":
+                    print()
+                    employeeMenu()
+            
 
+
+#MANAGER MENU CODE
 def managerMenu():
-    #MANAGER MENU CODE HERE
-    print("You are a manager")
+    print("--- Manager Menu ---\n")
+    print("What would you like to do?")
+    print("1. Full Employee Equipment Report")
+    print("2. Employee Information")
+    print("3. Equipment Information")
+    print("4. Checked-out Equipment Status")
+    print("5. Update an Existing User")
+    print("6. Add a New User")
+    print("7. Remove a User")
+    print("8. Update Existing Equipment")
+    print("9. Add Equipment")
+    print("10. Remove Equipment")
+    print("11. Log Out\n")
+    while True:
+        userChoice = input("Enter a menu option: ")
+        if userChoice == '1':
+            print("Full Employee Equipment Report:")
+            generateReport(reportType, userID = None)
+            print()
+            continue
+        elif userChoice == '2':
+            print("Employee Information:")
+            userID = input("Enter specific user ID: ")
+            getUser(userID)
+            print()
+            continue
+        elif userChoice == '3':
+            print("Equipment Information:")
+            equipID = input("Enter the Equipment ID: ")
+            getEquipment(equipID)
+            print()
+            continue
+        elif userChoice == '4':
+            print("Checked-out Equipment Status:")
+            equipID = input("Enter the Equipment ID: ")
+            isCheckedOut(equip)
+            print()
+            continue
+        elif userChoice == '5':
+            print()
+            
+            print()
+            continue
+        elif userChoice == '6':
+            print()
+            
+            print()
+            continue
+        elif userChoice == '7':
+            print()
+            
+            print()
+            continue
+        elif userChoice == '8':
+            print()
+            
+            print()
+            continue
+        elif userChoice == '9':
+            print()
+            
+            print()
+            continue
+        elif userChoice == '10':
+            print()
+            
+            print()
+            continue
+        elif userChoice == '11':
+            cont = input("\nLog Out? (y/n): ").lower()
+            while True:
+                if cont == "y":
+                    print("\nSuccessfully logged out.\n\n")
+                    main()
+                elif cont == "n":
+                    print()
+                    managerMenu()
+
+
 
 def firstLoginPasswordChange(userID):
 	print("This is your first time logging in. You must set your password.")
--- interface.py
+++ interface.py
@@ -1,2 +1,166 @@
+# import tkinter module 
+
 from tkinter import *
 import application
+  
+
+# creating main tkinter window/toplevel 
+
+root = Tk() 
+
+root.title('Login')  
+
+top = Toplevel() 
+
+  
+
+# this wil create a label widget 
+
+employeeID = Label(top, text = "Employee ID: ") 
+
+password = Label(top, text = "Password: ") 
+
+   
+
+# grid method to arrange labels in respective 
+
+# put label widget on the screen 
+
+employeeID.grid(row = 1, column = 0, sticky = W, pady = 2) 
+
+password.grid(row = 2, column = 0, sticky = W, pady = 2) 
+
+   
+
+# entry widgets, used to take entry from user 
+
+e1 = Entry(top) 
+
+e2 = Entry(top) 
+
+   
+
+# this will arrange input widgets 
+
+e1.grid(row = 1, column = 1, pady = 2) 
+
+e2.grid(row = 2, column = 1, pady = 2) 
+
+  
+  
+
+lBtn = Button(top, text = "LOGIN ", command=open) 
+
+# this will arrange the login button 
+
+lBtn.grid(row = 3, column = 0, padx = 50) 
+
+   
+
+#this will create a login button 
+
+#will take the user to the next page on click 
+
+#open function is allow the next window to open when "login" is clicked 
+
+#lbls of all tools to choose from 
+
+  
+
+#toplevel  
+
+top = Toplevel() 
+
+  
+
+#new window function to be called when button pressed 
+
+def new_window(): 
+
+    window = Toplevel(root) 
+
+    canvas = tk.Canvase(window, height=HEIGHT, width=WIDTH) 
+
+    canvas.pack() 
+
+  
+
+    HEIGHT = 400 
+
+    Widget = 300 
+
+    root = tk.Tk() 
+
+    root.title("Equipment Checkout") 
+
+    canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH) 
+
+    canvas.pack() 
+
+    button = tk.Button(root, text="Checkout", bg='black', fg='#469A00', 
+
+                       command=lambda: new_window()) 
+
+    button.grid() 
+
+
+
+def open(): 
+
+   # top = Toplevel() 
+
+    #top.title('Equipment Inventory') 
+
+    ##creates labels for the tool names 
+
+    #toolHacksaw = Label(top, text = "Hacksaw ") 
+
+    #toolHoleSawKit = Label(top, text = "Hole Saw Kit ") 
+
+    #toolHandAuger = Label(top, text = "Hand Auger ") 
+
+    #toolWeldingClamps = Label(top, text = "Welding Clamps") 
+
+    #toolAngleGrinder = Label(top, text = "Angle Grinder") 
+
+    ##grid method to arrange labels in respective 
+
+    ##put label widget on the screen 
+
+    #toolHacksaw.grid(row = 1, column = 0, sticky = W, pady = 2) 
+
+    #toolHoleSawKit.grid(row = 2, column = 0, sticky = W, pady = 2) 
+
+    #toolHandAuger.grid(row = 3, column = 0, sticky = W, pady = 2) 
+
+    #toolWeldingClamps.grid(row = 4, column = 0, sticky = W, pady = 2) 
+
+    #toolAngleGrinder.grid(row= 5, column = 0, sticky = W, pady = 2) 
+
+  
+
+     #check boxes to select tool to checkout 
+
+    var = IntVar() 
+
+    c=Checkbutton(root, text="HackSaw") 
+
+    c=Checkbutton(root, text="Hole Saw Kit") 
+
+    c=Checkbutton(root, text="Hand Auger") 
+
+    c=Checkbutton(root, text="Welding Clamps") 
+
+    c=Checkbutton(root, text="Angle Grinder") 
+
+  
+
+    lBtn = Button(root, text ="LOGIN", command=open) 
+
+
+
+# infinite loop which can be terminated by keyboard 
+
+# or mouse interrupt 
+
+mainloop() 
